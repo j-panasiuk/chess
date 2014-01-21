6f8adfe4f1a5ffad46c74e776073d6251d089d1d
daisy_yackovski <daisy.yackovski@gmail.com>
2014-01-21 21:39:52 +0100

    [engine] general update
    tree: increased default search depth to 2
    node: moved `evaluate` method from factory function to prototype object
    tree: removed `branch` property
    tree: renamed `expand` to `grow`
    tree.grow: added recursive `expand` function (from root upward)
    tree.analyze: added recursive `analyzeNode` function (from leaves backward)
    renamed `getEvaluation` function to `evaluation`

--- js/services/engine.js
+++ js/services/engine.js
@@ -2,9 +2,17 @@
 
 app.factory('engine', function(rules, game) {
 	console.log('%cCreating engine...', LOG.action);
+//
+//
 	var engine = {};
+//	Property 			Description
+//	----------------------------------------------------------------------------
+//	tree 				(Object) Represents chess game tree, with current position being
+//						the root and each possible becoming subsequent child node.
+//						Tree branching repeats recursively up to a given depth.
+
 //	Declare local variables.
-	var tree = {}, _node = {}, maxDepth = 1;
+	var tree = {}, _node = {}, maxDepth = 2;
 
 
 	Object.defineProperties(_node, {
@@ -15,7 +23,7 @@ app.factory('engine', function(rules, game) {
 		'children': 	{ writable: true, configurable: true, enumerable: true },
 		'value':		{ writable: true, configurable: true, enumerable: true },
 		'isRoot': 		{ get: function() { return this.depth === 0; } },
-		'isLeaf': 		{ get: function() { return this.depth === maxDepth; } },
+		'isLeaf': 		{ get: function() { return !this.children; } },
 		'isMax': 		{ get: function() { return !(this.depth % 2); } },
 		'isMin': 		{ get: function() { return !!(this.depth % 2); } },
 		'ramify': {
@@ -24,25 +32,34 @@ app.factory('engine', function(rules, game) {
 					self = this;
 
 				if (_.isArray(this.children)) {
-					console.log('%cThis node is already branched.', LOG.warn, node);
+					//console.log('%cThis node is already branched.', LOG.warn, node);
 					return;
 				}
-				console.debug(self.position.moves.length);
+				//console.debug(self.position.moves.length);
 				childNodes = this.position.moves.map(function(move) {
-					return createNode(self.position.yields(move), move, self);
+					return createNode(self.position.yields(move), move.san, self);
 				});
-				//.map(function(position) {
-				//	return createNode(position, self);
-				//});
 				this.children = childNodes;
-				console.log('%cTree node has ramified:', LOG.attention, this.children);
+				//console.log('%cTree node has ramified:', LOG.attention, this.children);
+			}
+		},
+		'evaluate': {
+			value: function() {
+				var childValues;
+
+				if (this.isLeaf) {
+					this.value = evaluation(this.position);
+				} else {
+					childValues = this.children.map(function(child) { return child.value; });
+					this.value = this.isMax ? _.max(childValues) : _.min(childValues);
+				}
 			}
 		}
 	});
 
 	function createNode(position, move, parentNode) {
-		console.assert(position.fen.match(rules.validFen), 'Invalid position.', position);
-		console.assert(_node.isPrototypeOf(parentNode) || (parentNode === undefined), 'Invalid parent node.', parentNode);
+		//console.assert(position.fen.match(rules.validFen), 'Invalid position.', position);
+		//console.assert(_node.isPrototypeOf(parentNode) || (parentNode === undefined), 'Invalid parent node.', parentNode);
 	//	Node factory function.
 	//	Creates a new game tree node, based on given postion.
 	//	If no parentNode is given, create tree root.
@@ -57,32 +74,115 @@ app.factory('engine', function(rules, game) {
 		node.parent = parentNode || null;
 		node.depth = parentNode ? parentNode.depth + 1 : 0;
 		node.children = null;
-		//node.evaluate = node.isLeaf ? evaluate : acquire;
-		Object.defineProperty(node, 'evaluate', {
-			value: function() {
-				var childValues;
-				if (this.isLeaf) {
-					this.value = getEvaluation(this.position);
-				} else {
-					childValues = this.children.map(function(node) { return node.value; });
-					this.value = this.isMax ? _.max(childValues) : _.min(childValues);
-				}
-				console.log('%cEvaluating node...', LOG.action, this.value);
-			}
-		});
 
 		return node;
 	}
 
-	function getEvaluation(position) {
+//	Define game tree.
+	Object.defineProperties(tree, {
+		'root': { value: null, writable: true, configurable: true, enumerable: true },
+		'plant': { 
+			value: function plantTree(position) {
+				console.log('%cPlanting game tree...', LOG.action);
+				this.root = createNode(position);
+			} 
+		},
+		'grow': {
+			value: function growTree() {
+				var nodes = 0;
+
+				console.log('%cExpanding game tree...', LOG.action);
+				console.time('Growing Tree');
+				expand(this.root);
+
+				function expand(currentNode) {
+					if (currentNode.depth >= maxDepth) {
+						nodes += 1;
+						return;
+					} else {
+						currentNode.ramify();
+						for (var i = 0; i < currentNode.children.length; i++) {
+							expand(currentNode.children[i]);
+						}
+						nodes += 1;
+					}					
+				}
+
+				console.timeEnd('Growing Tree');
+				console.debug('Grown nodes:', nodes);
+				console.log('%cExpanded tree...', LOG.attention, this);
+			}
+		},
+		'analyze': {
+			value: function analyzeTree() {
+				var nodes = 0;
+
+				console.log('%cAnalyzing tree...', LOG.action);
+				console.time('Analyzing Tree');
+				analyzeNode(this.root);
+
+				function analyzeNode(currentNode) {
+					if (currentNode.isLeaf) {
+						currentNode.evaluate();
+						nodes += 1;
+						return;
+					} else {
+						for (var i = 0; i < currentNode.children.length; i++) {
+							analyzeNode(currentNode.children[i]);
+						}
+						currentNode.evaluate();
+						nodes += 1;
+					}
+				}
+
+				console.time('Analyzing Tree');
+				console.debug('Analyzed nodes:', nodes);
+				console.log('%cTree root value:', LOG.state, this.root.value);
+			}
+		},
+		'getMove': {
+			value: function(position) {				
+				var childNodes, node, move;
+
+			//	Reset game tree to current position.
+				this.plant(position);
+
+			//	Branch out the tree up to given depth.
+				this.grow();
+
+			//	Assign values to all tree nodes.
+				this.analyze();
+
+			//	Sort best available moves, based on computed values.
+				childNodes = this.root.children;
+				childNodes = _.sortBy(childNodes, 'value');
+
+			//	Select highest or lowest valued move, depending on color.
+				node = game.activeColor ? _.first(childNodes) : _.last(childNodes);
+
+				console.debug('Best move:', node.move, node.value);
+				return node.move; 
+			}
+		}
+	});
+
+	function evaluation(position) {
 		console.assert(position.fen.match(rules.validFen), 'Invalid position.', position);
 	//	Evaluate position. Return computed value.
+		return _.sample(_.range(20));
+	/*
 		var 
 			pieces = position.pieceLists,
 			piecesWhite = pieces[0],
 			piecesBlack = pieces[1],
 			pawnsWhite = pieces.filter(17),
 			pawnsBlack = pieces.filter(25),
+			knightsWhite = pieces.filter(18),
+			knightsBlack = pieces.filter(26),
+			kingWhite = pieces.filter(19),
+			kingBlack = pieces.filter(27),
+			lightPiecesWhite = pieces[0].filter(function(piece) { return !!piece.isLight; }),
+			lightPiecesBlack = pieces[1].filter(function(piece) { return !!piece.isLight; }),
 			value = 0;
 
 	//	Count the material.
@@ -101,71 +201,32 @@ app.factory('engine', function(rules, game) {
 			value -= rules.ACTIVITY[25][pawnsBlack[i].square];
 		}
 
-	//	Pinned pieces.
+	//	Piece activity.
+		for (var i = 0; i < knightsWhite.length; i++) {
+			value += rules.ACTIVITY[18][knightsWhite[i].square];
+		}
+		for (var i = 0; i < knightsBlack.length; i++) {
+			value -= rules.ACTIVITY[26][knightsBlack[i].square];
+		}
+
+	//	Apply penalty for light pieces on the first rank.
+		for (var i = 0; i < lightPiecesWhite.length; i++) {
+			if (lightPiecesWhite[i].square.rank === 0) {
+				value -= 5;
+			}
+		}
+		for (var i = 0; i < lightPiecesBlack.length; i++) {
+			if (lightPiecesBlack[i].square.rank === 7) {
+				value += 5;
+			}
+		}
+
+	//	King safety.
 
 		console.log('%cEvaluating position...', LOG.action, value);
 		return value;
+	*/
 	}
-
-//	Define game tree.
-	Object.defineProperties(tree, {
-		'root': { value: null, writable: true, configurable: true, enumerable: true },
-		'branch': { value: [], writable: true, configurable: true, enumerable: true },
-		'plant': { 
-			value: function plantTree(position) {
-				console.log('%cPlanting game tree...', LOG.action);
-				this.root = createNode(position);
-				this.branch = [this.root];
-			} 
-		},
-		'expand': {
-			value: function expandBranch() {
-				console.log('%cExpanding game tree...', LOG.action);
-				var currentDepth = this.branch.length - 1;
-
-				this.branch[currentDepth].ramify();
-				console.log('%cExpanded tree...', LOG.attention, this);
-			}
-		},
-		'analyze': {
-			value: function analyzeLeaves() {
-				var childNode;
-				for (var i = 0; i < this.root.children.length; i++) {
-					childNode = this.root.children[i];
-					childNode.evaluate();
-				}
-				console.log('%cAnalyzing root value...', LOG.action);
-				this.root.evaluate();
-				console.log('%cRoot value:', LOG.state, this.root.value);
-			}
-		},
-		'getMove': {
-			value: function(position) {
-				console.time('Compute move');
-				var childNodes, node, move;
-
-			//	Reset game tree to current position.
-				this.plant(position);
-
-			//	Branch out the tree up to given depth.
-				this.expand();
-
-			//	Assign values to all tree nodes.
-				this.analyze();
-
-			//	Sort best available moves, based on computed values.
-				childNodes = this.root.children;
-				childNodes = _.sortBy(childNodes, 'value');
-
-			//	Select highest or lowest valued move, depending on color.
-				node = game.activeColor ? _.first(childNodes) : _.last(childNodes);
-
-				console.timeEnd('Compute move');
-				console.log('%cBest move:', LOG.attention, node.move.san);
-				return node.move; 
-			}
-		}
-	});
 
 	engine.tree = tree;
 	T = tree;
