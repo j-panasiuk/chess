024d4e6fe3007afed73366c85e127923d37ec568
daisy_yackovski <daisy.yackovski@gmail.com>
2014-02-09 12:59:43 +0100

    [testing] updated unit test for 'settings' service

--- test/unit/settings.spec.js
+++ test/unit/settings.spec.js
@@ -12,14 +12,24 @@ describe('Settings service:', function() {
         expect($settings).toBeDefined();
     });
 
-    it('should be have legal values', function() {
+    it('should have correct defaults', function() {
         expect(_.values($settings.CONTROL_FLAGS)).toContain($settings.controlWhite);
         expect(_.values($settings.CONTROL_FLAGS)).toContain($settings.controlBlack);
-        expect(typeof $settings.difficulty).toEqual('number');
+        expect(typeof $settings.fen).toEqual('string');
         expect(typeof $settings.isReversed).toEqual('boolean');
         expect(typeof $settings.reverseForBlack).toEqual('boolean');
         expect(typeof $settings.autoRestart).toEqual('boolean');
         expect(typeof $settings.switchColorOnRestart).toEqual('boolean');
+        expect(typeof $settings.highlightChecks).toEqual('boolean');
+        expect(typeof $settings.highlightLastMove).toEqual('boolean');
+        expect(typeof $settings.moveList).toEqual('boolean');
+        expect(typeof $settings.moveEvaluation).toEqual('boolean');
+    });
+
+    it('should have correct accessor methods', function() {
+        expect($settings.all.length).toEqual(_.size($settings.defaultSettings));
+        expect(typeof $settings.reset).toEqual('function');
+        expect(typeof $settings.switchControls).toEqual('function');
     });
 
 });
